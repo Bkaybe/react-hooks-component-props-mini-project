b4dad31b5b3bd919e9dbf88bea560b71f9267f0a
Bethuel Khisa <sunguribethuel@gmail.com>
2024-08-13 06:16:13 +0300

    Finished

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,15 @@
 import React from "react";
 
-const About = ({ about, image = "https://via.placeholder.com/215" }) => {
-  return (
-    <aside>
-      <img src={image} alt="blog log" />
-      <p>{about}</p>
-    </aside>
-  );
-};
+const About = ({about, image = "https://via.placeholder.com/215"}) => {
+    return (
+        <div>
+            <aside>
+                <img src={image} alt={"blog logo"} />
+                <p>{about}</p>
+            </aside>
+        </div>
+    )
+}
+
 
 export default About;
